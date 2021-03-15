ac94a8a01537aaa429a39779e9acafb7d16d1dbe
Luiz Henrique <lmenezesmessias@gmail.com>
2021-03-15 20:01:10 -0300

    Finalização

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Para converter uma temperatura digitada pelo usuário em °F para °C, digite: ");
+
+            Console.WriteLine("a temperatura em°F: ");
+            double f = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = (f - 32)/1.8; 
+
+            Console.WriteLine($"A temperatura convertida dá: {resultado:N2} °F"); 
+        }
+    }
+}
